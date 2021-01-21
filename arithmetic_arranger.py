31e5d04673b32f3c315d4b1c1a4cb2b834bfb87b
Daniel Melody <dmwalter12@gmail.com>
2021-01-20 21:45:23 -0500

    initial commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,20 @@
-def arithmetic_arranger(problems):
+import sys
 
+
+def arithmetic_arranger(problems):
+    #! print error message if there are more than 5 problems
+    if len(problems) > 5:
+        print("Error: Too many problems.")
+        sys.exit()
+
+    answers = []  # array to store answers to the problems
+    for prob in problems:
+        split = prob.split()
+        lengths = {}  # key = string, value = length
+        for i in range(len(split)):
+            elt_length = len(split[i])
+            lengths.append(split[i], elt_length)
+
+        lengths
 
     return arranged_problems
